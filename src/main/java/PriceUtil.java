01b86fe73ddb6e900a5399184135f91ebef2c51d
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2012-04-20 10:48:40 +0200

    Initial import.

---
+++ src/main/java/PriceUtil.java
@@ -0,0 +1,9 @@
+public class PriceUtil {
+  /**
+   * @return TRUE, if 'newPriceInCent' justifies labeling the article as an "red pencil promotion"
+   */
+  public static boolean isRedPencilPromotion(int oldPriceInCent, int newPriceInCent) {
+    return false;
+  }
+}
+
