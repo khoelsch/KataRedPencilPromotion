98625954edb46268ab76ec3817343ca6785d9d54
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2012-04-20 12:50:38 +0200

    First, simple test succeeds

--- src/main/java/PriceUtil.java
+++
@@ -1,9 +0,0 @@
-public class PriceUtil {
-  /**
-   * @return TRUE, if 'newPriceInCent' justifies labeling the article as an "red pencil promotion"
-   */
-  public static boolean isRedPencilPromotion(int oldPriceInCent, int newPriceInCent) {
-    return false;
-  }
-}
-
