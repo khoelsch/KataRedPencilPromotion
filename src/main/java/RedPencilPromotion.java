8558a994985eb87b51125046b8e3a85f602ba2dc
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2012-06-08 12:14:42 +0200

    More green tests

--- src/main/java/RedPencilPromotion.java
+++ src/main/java/RedPencilPromotion.java
@@ -1,12 +1,12 @@
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class RedPencilPromotion {
-  public final static float CHANGE_RATE_INTERVAL_START = 0.95f;
-  public final static float CHANGE_RATE_INTERVAL_END = 0.70f;
+  public final static float PRICE_FACTOR_INTERVAL_START = 0.95f;
+  public final static float PRICE_FACTOR_INTERVAL_END = 0.70f;
+  public final static int DAYS_PRICE_MUST_BE_STABLE = 30;
 
-  private final static int DAYS_PROMOTION_DURATION = 30;
-  private final static int DAYS_PRICE_MUST_BE_STABLE = 30;
   private final List<PriceChange> appliedPriceChanges;
   private final Date dateOfEvaluation;
 
@@ -22,8 +22,8 @@ public class RedPencilPromotion {
 
     PriceChange priorPriceChange = null;
     for (PriceChange currentPriceChange : appliedPriceChanges) {
-      promotionIsActive = changeRateWithinValueInterval(currentPriceChange);
-              //&& priceWasStable(priorPriceChange, currentPriceChange);
+      promotionIsActive = changeRateWithinValueInterval(currentPriceChange)
+              && priceWasStable(priorPriceChange, currentPriceChange);
 
       priorPriceChange = currentPriceChange;
     }
@@ -32,24 +32,23 @@ public class RedPencilPromotion {
   }
 
   private boolean changeRateWithinValueInterval(PriceChange priceChange) {
-    boolean fitsLowerBound = Float.compare(priceChange.priceFactor, CHANGE_RATE_INTERVAL_START) <= 0;
-    boolean fitsUpperBound = Float.compare(priceChange.priceFactor, CHANGE_RATE_INTERVAL_END) >= 0;
+    boolean fitsLowerBound = Float.compare(priceChange.priceFactor, PRICE_FACTOR_INTERVAL_START) <= 0;
+    boolean fitsUpperBound = Float.compare(priceChange.priceFactor, PRICE_FACTOR_INTERVAL_END) >= 0;
 
     return fitsLowerBound && fitsUpperBound;
   }
 
-  private boolean fitsPromotionInterval() {
-    //Date firstPriceChange = appliedPriceChanges.get(0).changeDate;
-    return false;
+  private boolean priceWasStable(PriceChange priorPriceChange, PriceChange currentPriceChange) {
+    if (priorPriceChange == null) {
+      return true;
+    }
 
-    //return firstPriceChange.changeDate.compareTo(dateOfEvaluation) <= 0
-  }
+    final Calendar cal =  Calendar.getInstance();
+    cal.setTime(currentPriceChange.changeDate);
+    cal.add(Calendar.DAY_OF_MONTH, -1 * RedPencilPromotion.DAYS_PRICE_MUST_BE_STABLE);
+    final Date earliestValidChangeDate = cal.getTime();
 
-  /**
-   * @return the last Date this "red pencil promotion" will be valid for
-   */
-  public Date getIntervalEnd() {
-    return null;
+    return priorPriceChange.changeDate.compareTo(earliestValidChangeDate) <= 0;
   }
 }
 
--- src/test/java/RedPencilPromotionTest.java
+++ src/test/java/RedPencilPromotionTest.java
@@ -10,91 +10,109 @@ import java.util.List;
 public class RedPencilPromotionTest {
   private static final String MSG_PRICE_NOT_REDUCED = "Price NOT reduced, although it should be!";
   private static final String MSG_PRICE_REDUCED = "Price IS reduced, although it shouldn't!";
-  private static final int DAYS_TO_GO_BACK_WITHIN_PROMOTION_INTERVAL = 10;
+  private static final int ACTIVE_PROMOTION_DAYS_TO_GO_BACK = RedPencilPromotion.DAYS_PRICE_MUST_BE_STABLE + 2;
+  private static final float ACTIVE_PROMOTION_PRICE_FACTOR = RedPencilPromotion.PRICE_FACTOR_INTERVAL_START;
+
   private RedPencilPromotion rpp;
   private List<PriceChange> priceChanges;
 
   @Test
-  public void isActive_noPriceChanges() {
+  public void notActive_noPriceChanges() {
     priceChanges = Collections.emptyList();
 
     rpp = new RedPencilPromotion(priceChanges, nowDate());
 
-    Assert.assertEquals(MSG_PRICE_REDUCED, false, rpp.isActive());
+    assertPromotionNotActive();
   }
 
   @Test
-  public void isActive_singlePriceChangeWithinPromotionInterval() {
-    PriceChange priceChange = new PriceChange(0.8f, nDaysBackFromNow(DAYS_TO_GO_BACK_WITHIN_PROMOTION_INTERVAL));
+  public void isActive_singlePriceChangeWithinPriceFactorInterval() {
+    PriceChange priceChange = new PriceChange(0.8f, nDaysBackFromNow(ACTIVE_PROMOTION_DAYS_TO_GO_BACK));
     priceChanges = Arrays.asList(priceChange);
 
     rpp = new RedPencilPromotion(priceChanges, nowDate());
 
-    Assert.assertEquals(MSG_PRICE_NOT_REDUCED, true, rpp.isActive());
+    assertPromotionIsActive();
   }
 
   @Test
-  public void isActive_singlePriceChangeOnPromotionIntervalStart() {
-    final float priceFactor = RedPencilPromotion.CHANGE_RATE_INTERVAL_START;
-    final Date changeDate = nDaysBackFromNow(DAYS_TO_GO_BACK_WITHIN_PROMOTION_INTERVAL);
+  public void isActive_singlePriceChangeOnPriceFactorIntervalStart() {
+    final float priceFactor = RedPencilPromotion.PRICE_FACTOR_INTERVAL_START;
+    final Date changeDate = nDaysBackFromNow(ACTIVE_PROMOTION_DAYS_TO_GO_BACK);
 
     PriceChange priceChange = new PriceChange(priceFactor, changeDate);
     priceChanges = Arrays.asList(priceChange);
 
     rpp = new RedPencilPromotion(priceChanges, nowDate());
 
-    Assert.assertEquals(MSG_PRICE_NOT_REDUCED, true, rpp.isActive());
+    assertPromotionIsActive();
   }
 
   @Test
-  public void isActive_singlePriceChangeOnPromotionIntervalEnd() {
-    final float priceFactor = RedPencilPromotion.CHANGE_RATE_INTERVAL_END;
-    final Date changeDate = nDaysBackFromNow(DAYS_TO_GO_BACK_WITHIN_PROMOTION_INTERVAL);
+  public void isActive_singlePriceChangeOnPriceFactorIntervalEnd() {
+    final float priceFactor = RedPencilPromotion.PRICE_FACTOR_INTERVAL_END;
+    final Date changeDate = nDaysBackFromNow(ACTIVE_PROMOTION_DAYS_TO_GO_BACK);
 
     PriceChange priceChange = new PriceChange(priceFactor, changeDate);
     priceChanges = Arrays.asList(priceChange);
 
     rpp = new RedPencilPromotion(priceChanges, nowDate());
 
-    Assert.assertEquals(MSG_PRICE_NOT_REDUCED, true, rpp.isActive());
+    assertPromotionIsActive();
   }
 
   @Test
-  public void notActive_singlePriceChangeBeforePromotionIntervalStart() {
-    final float priceFactor = RedPencilPromotion.CHANGE_RATE_INTERVAL_START + 0.1f;
-    final Date changeDate = nDaysBackFromNow(DAYS_TO_GO_BACK_WITHIN_PROMOTION_INTERVAL);
+  public void notActive_singlePriceChangeBeforePriceFactorIntervalStart() {
+    final float priceFactor = RedPencilPromotion.PRICE_FACTOR_INTERVAL_START + 0.1f;
+    final Date changeDate = nDaysBackFromNow(ACTIVE_PROMOTION_DAYS_TO_GO_BACK);
 
     PriceChange priceChange = new PriceChange(priceFactor, changeDate);
     priceChanges = Arrays.asList(priceChange);
 
     rpp = new RedPencilPromotion(priceChanges, nowDate());
 
-    Assert.assertEquals(MSG_PRICE_REDUCED, false, rpp.isActive());
+    assertPromotionNotActive();
   }
 
   @Test
-  public void notActive_singlePriceChangeBeforePromotionIntervalEnd() {
-    final float priceFactor = RedPencilPromotion.CHANGE_RATE_INTERVAL_END - 0.1f;
-    final Date changeDate = nDaysBackFromNow(DAYS_TO_GO_BACK_WITHIN_PROMOTION_INTERVAL);
+  public void isActive_singlePriceChangeAfterPriceFactorIntervalEnd() {
+    final float priceFactor = RedPencilPromotion.PRICE_FACTOR_INTERVAL_END - 0.1f;
+    final Date changeDate = nDaysBackFromNow(ACTIVE_PROMOTION_DAYS_TO_GO_BACK);
 
     PriceChange priceChange = new PriceChange(priceFactor, changeDate);
     priceChanges = Arrays.asList(priceChange);
 
     rpp = new RedPencilPromotion(priceChanges, nowDate());
 
-    Assert.assertEquals(MSG_PRICE_REDUCED, false, rpp.isActive());
+    assertPromotionNotActive();
   }
 
+  @Test
+  public void notActive_priceNotStable() {
+    final float priceFactor1 = ACTIVE_PROMOTION_PRICE_FACTOR;
+    PriceChange pc1 = new PriceChange(priceFactor1, nDaysBackFromNow(3));
 
+    final float priceFactor2 = ACTIVE_PROMOTION_PRICE_FACTOR;
+    PriceChange pc2 = new PriceChange(priceFactor2, nowDate());
 
-  private static Date nowDate() {
-    return Calendar.getInstance().getTime();
+    priceChanges = Arrays.asList(pc1, pc2);
+    rpp = new RedPencilPromotion(priceChanges, nowDate());
+
+    assertPromotionNotActive();
   }
 
-  private static Date nDaysBackFromNow(int daysToGoBack) {
-    Calendar cal = Calendar.getInstance();
-    cal.add(Calendar.DAY_OF_MONTH, -1 * daysToGoBack);
-    return cal.getTime();
+  @Test
+  public void isActive_priceIsStable() {
+    final float priceFactor1 = ACTIVE_PROMOTION_PRICE_FACTOR;
+    PriceChange pc1 = new PriceChange(priceFactor1, nDaysBackFromNow(ACTIVE_PROMOTION_DAYS_TO_GO_BACK));
+
+    final float priceFactor2 = ACTIVE_PROMOTION_PRICE_FACTOR;
+    PriceChange pc2 = new PriceChange(priceFactor2, nowDate());
+
+    priceChanges = Arrays.asList(pc1, pc2);
+    rpp = new RedPencilPromotion(priceChanges, nowDate());
+
+    assertPromotionIsActive();
   }
 
   /*
@@ -110,6 +128,7 @@ public class RedPencilPromotionTest {
   }
   */
 
+
   /*
   @Test
   public void isActive_singlePriceChangeOutsideInterval() {
@@ -123,4 +142,26 @@ public class RedPencilPromotionTest {
     Assert.assertEquals(MSG_PRICE_REDUCED, false, rpp.isActive());
   }
   */
+
+  // --------------
+  // Helper methods
+  // --------------
+
+  private void assertPromotionNotActive() {
+    Assert.assertEquals(MSG_PRICE_REDUCED, false, rpp.isActive());
+  }
+
+  private void assertPromotionIsActive() {
+    Assert.assertEquals(MSG_PRICE_NOT_REDUCED, true, rpp.isActive());
+  }
+
+  private static Date nowDate() {
+    return Calendar.getInstance().getTime();
+  }
+
+  private static Date nDaysBackFromNow(int daysToGoBack) {
+    Calendar cal = Calendar.getInstance();
+    cal.add(Calendar.DAY_OF_MONTH, -1 * daysToGoBack);
+    return cal.getTime();
+  }
 }
